cfce8b38b91ca0d64230cbc2db2b3b56e21c5e34
sadia-jannat <37320382+sadia-jannat@users.noreply.github.com>
2022-12-11 19:10:25 +0600

    Update delete_calendar.py

--- delete_calendar.py
+++ delete_calendar.py
@@ -8,9 +8,8 @@ SCOPES=['https://www.googleapis.com/auth/calendar']
 
 service=Create_Service(CLIENT_SECRET_FILE,API_NAME,API_VERSION,SCOPES)
 
-request_body={
-    'summary':'calendar event'
+#request_body={
+#   'summary':'calendar event'}
 
-}
 #to delete calendar
 service.calendars().delete(calendarId='8l3v0tc3hbhg145agifhfgip7s@group.calendar.google.com').execute()
